863f9a2fa6e979bb2871685188ff48a7892c6ee3
kangand <andrea.kang@gmail.com>
2020-07-06 23:55:10 -0400

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://www.googleapis.com/drive/v3/files/1-3A3bNr6Fn0cLaCR9d6XJkwgBMGzJkkC?alt=media&key=AIzaSyAEL35rBIAw3pyfMBa8t9lsNqwVkd-9H3I'
+export_file_name = 'squeezenet_model'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['bracelet', 'earrings', 'necklace', 'ring]
 path = Path(__file__).parent
 
 app = Starlette()
